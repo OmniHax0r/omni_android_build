5819a0f8a203fc3683bb45d7dd26a006d909cef9
Geofferey Eakins <geofferey.eakins@gmail.com>
2020-07-19 01:47:36 -0700

    releasetools: Regenerate hashtree for prebuilt odm.img files
    When building with a prebuilt odm.img file (via BOARD_PREBUILT_ODMIMAGE)
    the odm.img may not have the same hashtree and salt as the ones being
    created during the build process.
    
    To make sure that odm.img matches system/boot/dtbo, the hashtree should be
    rewritten using avbtool.
    
    This patch therefor adds in the rewrite of the hashtree if using a prebuilt
    odm.img and AVB is enabled.  The code is similar to the code in the
    Add_Dtbo() function in this same script.
    releasetools: Regenerate hashtree for prebuilt odm.img files
    
    Change-Id: I365491aabbe51e4757bfc9f1cf317e07306300a6

--- tools/releasetools/add_img_to_target_files.py
+++ tools/releasetools/add_img_to_target_files.py
@@ -249,6 +249,20 @@ def AddOdm(output_zip):
   img = OutputFile(output_zip, OPTIONS.input_tmp, "IMAGES", "odm.img")
   if os.path.exists(img.name):
     logger.info("odm.img already exists; no need to rebuild...")
+
+    # AVB-sign the image as needed.
+    if OPTIONS.info_dict.get("avb_enable") == "true":
+      logger.info("updating avb hash for prebuilt odm.img...")
+      avbtool = OPTIONS.info_dict["avb_avbtool"]
+      # The AVB hash footer will be replaced if already present.
+      cmd = [avbtool, "add_hashtree_footer", "--image", img.name,
+             "--partition_name", "odm"]
+      common.AppendAVBSigningArgs(cmd, "odm")
+      args = OPTIONS.info_dict.get("avb_odm_add_hashtree_footer_args")
+      if args and args.strip():
+        cmd.extend(shlex.split(args))
+      common.RunAndCheckOutput(cmd)
+
     return img.name
 
   block_list = OutputFile(
